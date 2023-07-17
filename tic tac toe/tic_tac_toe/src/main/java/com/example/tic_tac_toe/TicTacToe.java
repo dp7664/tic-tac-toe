c92d038b6b6cf0dcc09d7d66e602e6908ae33eb2
dp7664 <124852712+dp7664@users.noreply.github.com>
2023-07-17 22:14:50 +0530

    Add files via upload

---
+++ tic tac toe/tic_tac_toe/src/main/java/com/example/tic_tac_toe/TicTacToe.java
@@ -0,0 +1,214 @@
+package com.example.tic_tac_toe;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.HBox.*;
+
+
+public class TicTacToe extends Application {
+
+    private Label playerXScoreLabel,playerOScoreLabel;
+
+    private int playerXScore=0,playerOScore=0;
+
+    private Button buttons[][]=new Button[3][3];
+
+    private boolean playerXTurn=true;
+
+    private BorderPane createContent(){
+        BorderPane root=new BorderPane();
+        root.setPadding(new Insets(20,20,20,20));
+
+    //title
+        Label titleLabel=new Label("Tic  Tac  Toe");
+        titleLabel.setStyle("-fx-font-size:40pt;-fx-font-weight:bold;");
+        root.setTop(titleLabel);
+        BorderPane.setAlignment(titleLabel,Pos.CENTER);
+
+
+        //GameBoard
+        GridPane gridPane=new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.CENTER);
+        for(int i = 0; i < 3; i++) {
+          for(int j = 0;j < 3; j++){
+
+             Button button=new Button();
+             button.setPrefSize(100,100);
+
+             button.setOnAction(event->buttonClicked(button));
+             button.setStyle("-fx-font-size:24pt;-fx-font-weight:bold;");
+              buttons[i][j]=button;
+             gridPane.add(button, j, i);
+          }
+        }
+        root.setCenter(gridPane);
+        BorderPane.setAlignment(gridPane,Pos.CENTER);
+
+        //scoreboard
+        HBox scoreBoard=new HBox(20);
+        scoreBoard.setAlignment(Pos.CENTER);
+        playerXScoreLabel=new Label("Player X:0");
+        playerXScoreLabel.setStyle("-fx-font-size:16pt;-fx-font-weight:bold;");
+
+        playerOScoreLabel = new Label("Player O:0");
+        playerOScoreLabel.setStyle("-fx-font-size:16pt;-fx-font-weight:bold;");
+
+        scoreBoard.getChildren().addAll(playerXScoreLabel, playerOScoreLabel);
+        root.setBottom(scoreBoard);
+
+        return root;
+    }
+    private void buttonClicked(Button button){
+        if(button.getText().equals("")){
+            if(playerXTurn){
+                button.setText("X");
+            }
+            else{
+                button.setText("O");
+            }
+            playerXTurn=!playerXTurn;
+            checkWinner();
+        }
+      return;
+    }
+
+    private void checkWinner(){
+        //row
+        for(int row=0;row<3;row++){
+            if(buttons[row][0].getText().equals(buttons[row][1].getText())
+                    &&(buttons[row][1].getText().equals(buttons[row][2].getText())
+                    &&!buttons[row][0].getText().isEmpty()))
+            {
+                //we will winner
+
+                String Winner=buttons[row][0].getText();
+                showWinnerDialog(Winner);
+
+                updateScore(Winner);
+                resetBoard();
+                return;
+            }
+        }
+
+
+        //column
+        for(int col=0;col<3;col++){
+            if(buttons[0][col].getText().equals(buttons[1][col].getText())
+                    &&(buttons[1][col].getText().equals(buttons[2][col].getText())
+                    &&!buttons[0][col].getText().isEmpty()))
+            {
+                //we will winner
+
+                String Winner=buttons[0][col].getText();
+                showWinnerDialog(Winner);
+                updateScore(Winner);
+                resetBoard();
+                return;
+            }
+        }
+
+
+        //diagonal
+        if(buttons[0][0].getText().equals(buttons[1][1].getText())
+                &&(buttons[1][1].getText().equals(buttons[2][2].getText())
+                &&!buttons[0][0].getText().isEmpty()))
+        {
+            //we will winner
+
+            String Winner=buttons[0][0].getText();
+            showWinnerDialog(Winner);
+            updateScore(Winner);
+            resetBoard();
+            return;
+        }
+        if(buttons[0][2].getText().equals(buttons[1][1].getText())
+                &&(buttons[1][1].getText().equals(buttons[2][0].getText())
+                &&!buttons[0][2].getText().isEmpty()))
+        {
+            //we will winner
+            String Winner=buttons[0][2].getText();
+            showWinnerDialog(Winner);
+
+            updateScore(Winner);
+            resetBoard();
+            return;
+        }
+        //tie
+        boolean tie=true;
+        for(Button row[]:buttons){
+            for(Button button:row){
+              if(button.getText().isEmpty()){
+                  tie=false;
+                  break;
+              }
+            }
+        }
+        if(tie){
+            showTieDialog();
+            resetBoard();
+        }
+    }
+
+    private void showWinnerDialog(String Winner){
+        Alert alert=new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Winner");
+        alert.setContentText(("Congratulations"+ Winner +" ! You won the game"));
+        alert.setHeaderText("");
+        alert.showAndWait();
+    }
+
+    private void showTieDialog(){
+        Alert alert=new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Tie");
+        alert.setContentText(("Game Over! its a Tie"));
+        alert.setHeaderText("");
+        alert.showAndWait();
+    }
+      private void updateScore(String Winner){
+        if(Winner.equals("X")){
+            playerXScore++;
+            playerXScoreLabel.setText("PlayerX: " + playerXScore);
+        }
+        else{
+            playerOScore++;
+            playerOScoreLabel.setText("Player O: " +playerOScore);
+        }
+      }
+
+      private void resetBoard(){
+        for(Button row[]:buttons){
+            for(Button button:row){
+                button.setText("");
+            }
+        }
+      }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(createContent());
+        stage.setTitle("Tic Tac Toe");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch();
+    }
+}
